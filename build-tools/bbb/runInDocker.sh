7430797a887180bf3e5a061f868fc136fd16e3c0
Henry Hoegelow <henry@hoegelow.com>
2020-09-16 21:44:03 +0200

    WIP

--- build-tools/bbb/runInDocker.sh
+++ build-tools/bbb/runInDocker.sh
@@ -8,28 +8,50 @@ DOCKER_ARGS="$1"
 shift
 
 SSH_DIR=$(realpath "$HOME/.ssh")
+TMP_DIR="$BINARY_DIR/tmp"
 
-TMPSCRIPT=$(mktemp)
-TMPSCRIPTBASE=$(basename $TMPSCRIPT)
-TMPDIRNAME="$BINARY_DIR/tmp"
-TMPSCRIPT="$TMPDIRNAME/$TMPSCRIPTBASE"
-mkdir -p $TMPDIRNAME
+ROOT_SCRIPT_BASE="root-script.sh"
+ROOT_SCRIPT_FILE="$TMP_DIR/$ROOT_SCRIPT_BASE"
+
+USER_SCRIPT_BASE="bob-script.sh"
+USER_SCRIPT_FILE="$TMP_DIR/$USER_SCRIPT_BASE"
+
+USER_ID=$(id -u $USER)
+
+mkdir -p $TMP_DIR
+
+echo "#!/bin/bash" > $USER_SCRIPT_FILE
+echo "set -x" >> $USER_SCRIPT_FILE
+for var in "$@"
+do
+    echo "$var" >> $USER_SCRIPT_FILE
+done
 
 # COPY local ssh files into the docker container to be able to push onto github
 echo "
+set -x
 mkdir -p /docker-ssh
 rsync -a /host-ssh/ /docker-ssh
 chown -R root /docker-ssh
 if ! grep \"/docker-ssh\" /etc/ssh/ssh_config; then
  echo \"IdentityFile /docker-ssh/id_rsa\" >> /etc/ssh/ssh_config
 fi
-set -e
-" > $TMPSCRIPT
 
-for var in "$@"
-do
-    echo $var >> $TMPSCRIPT
-done
+adduser bob-the-builder --uid $USER_ID --disabled-password --gecos \"\"
+echo \"bob-the-builder   ALL=(ALL) NOPASSWD:ALL\" >> /etc/sudoers
+
+chmod +x /script/bob-script.sh
+sudo -i -u bob-the-builder /script/bob-script.sh
+" > $ROOT_SCRIPT_FILE
+
 
 DOCKERNAME="nl-cross-build-environment"
-docker run $DOCKER_ARGS --privileged --rm -v $SSH_DIR:/host-ssh -v $TMPDIRNAME:/script -v $BINARY_DIR:/workdir -v $SOURCE_DIR:/sources $DOCKERNAME bash /script/$TMPSCRIPTBASE
+docker run $DOCKER_ARGS \
+    --privileged \
+    --rm \
+    -ti \
+    -v $SSH_DIR:/host-ssh \
+    -v $TMP_DIR:/script \
+    -v $BINARY_DIR:/workdir \
+    -v $SOURCE_DIR:/sources \
+    $DOCKERNAME bash -e /script/$ROOT_SCRIPT_BASE
--- build-tools/epc/runScriptInDocker.sh
+++ build-tools/epc/runScriptInDocker.sh
@@ -4,18 +4,27 @@ BINARY_DIR=$(realpath $1)
 SOURCE_DIR=$(realpath $2)
 SCRIPT=$(realpath $3)
 
-TMPSCRIPT=$(mktemp)
-TMPSCRIPTBASE=$(basename $TMPSCRIPT)
-TMPDIRNAME="$BINARY_DIR/tmp"
-TMPSCRIPT="$TMPDIRNAME/$TMPSCRIPTBASE"
-mkdir -p $TMPDIRNAME
-
+TMP_DIR="$BINARY_DIR/tmp"
 DOCKERNAME="nl-epc-build-environment"
 
-cp $SCRIPT $TMPSCRIPT
+ROOT_SCRIPT_BASE="root-script.sh"
+USER_SCRIPT_BASE="bob-script.sh"
 
-GROUP_ID=$(id -g $USER)
+ROOT_SCRIPT_FILE="$TMP_DIR/$ROOT_SCRIPT_BASE"
+USER_SCRIPT_FILE="$TMP_DIR/$USER_SCRIPT_BASE"
+
 USER_ID=$(id -u $USER)
 
-docker run -e GROUP_ID=$GROUP_ID -e USER_ID=$USER_ID --privileged --rm -v $TMPDIRNAME:/script -v $BINARY_DIR:/bindir -v $SOURCE_DIR:/sources \
-$DOCKERNAME bash /script/$TMPSCRIPTBASE
+mkdir -p $TMP_DIR
+cp $SCRIPT $USER_SCRIPT_FILE
+
+echo "
+set -x
+adduser bob-the-builder --uid $USER_ID --disabled-password --gecos \"\"
+echo \"bob-the-builder   ALL=(ALL) NOPASSWD:ALL\" >> /etc/sudoers
+
+chmod +x /script/bob-script.sh
+sudo -i -u bob-the-builder /script/bob-script.sh
+" > $ROOT_SCRIPT_FILE
+
+docker run --privileged --rm -v $TMP_DIR:/script -v $BINARY_DIR:/bindir -v $SOURCE_DIR:/sources $DOCKERNAME bash /script/$ROOT_SCRIPT_BASE
